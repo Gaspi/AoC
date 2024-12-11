80786027b51a3941a792f239584c0811845798dc
Gaspard FEREY <gaspard.ferey@gmail.com>
2024-12-11 09:29:05 +0100

    day01: Optimized solution

--- 2024/day1/aoc_2024_01.py
+++ 2024/day1/aoc_2024_01.py
@@ -2,9 +2,17 @@
 with open('./input_aoc_2024_01.txt', 'r') as file:
     data = [ [int(x) for x in l.split('   ')] for l in file.read().splitlines()]
 
-datal = sorted([x[0] for x in data])
-datar = sorted([x[1] for x in data])
+# Complexity: n.log(n)
+left  = sorted([x[0] for x in data])
+right = sorted([x[1] for x in data])
 
-print( sum( abs(a-b) for (a,b) in zip(datal, datar) ) )
+# Complexity: n
+print( sum( abs(a-b) for (a,b) in zip(left, right) ) )
 
-print( sum(a for a in datal for b in datar if a == b) )
+# Complexity: n²
+print( sum(a for a in left for b in right if a == b) )
+
+# Complexity: n.log(n)
+from collections import Counter
+right_count = Counter(right)
+print( sum(a * right_count[a] for a in left) )
